248c4f8f7ed7c1ca27e2c3f354b7077499ab0b24
robyng <robyngraham01@gmail.com>
2022-01-26 11:33:49 -0800

    get all users in user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,16 @@
 const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUser(req,res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+
+    // get one user by id
+}
