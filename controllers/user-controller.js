7f33d72f01c44a21a7634dd4d9bc092bff1a4373
robyng <robyngraham01@gmail.com>
2022-01-26 11:36:22 -0800

    add create user to controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,4 +13,11 @@ const userController = {
 
 
     // get one user by id
+
+    // create a user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    }
 }
