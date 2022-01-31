6972bc8242992837bdb76ef0136ba1e6773d8389
robyng <robyngraham01@gmail.com>
2022-01-30 21:10:54 -0800

    add delete, create and update thought controller and thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -19,17 +19,58 @@ getThoughtById(req, res) {
         res.status(400).json(err);
     });
 },
+
 // create new thought and push the created thought's id to the user's thought array
-createThought({body},res) {
-    // body is "thoughtText": "My String", "username": "robyng", "reactions": "", "reactionCount": ""
-    Thought.create({body})
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => res.status(400).json(err));
-}
+createThought({ params, body }, res) {
+        // body is "thoughtText": "My String", "username": "robyng", "reactions": "", "reactionCount": ""
+    Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: _id }},
+                {new: true}
+            );
+        })
+        .then(dbData => {
+            if(!dbData) {
+                res.status(404).json({ message: 'no user with this id '});
+                return;
+            }
+            res.json(dbData);
+        })
+        .catch(err => res.json(err));
+},
 
 // update a thought by its id
+updateOneThought(req,res) {
+    Thought.findByIdAndUpdate(req.params.id, req.body)
+    .then(dbUserData => {
+        if (!dbUserData) {
+          res.json({ message: 'No thought found by this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+},
 
 // delete thought by id
+deleteThought(req,res) {
+    Thought.findByIdAndDelete(req.params.id, req.body)
+    .then(dbUserData => {
+        if (!dbUserData) {
+          res.json({ message: 'No thought found by this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+
+}
 
 
 }
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,13 +3,23 @@ const router = require('express').Router();
 const {
     getAllThoughts,
     getThoughtById,
+    createThought,
+    updateOneThought,
+    deleteThought
   } = require('../../controllers/thought-controller');
 
 
-
+//thoughts
   router
     .route('/')
     .get(getAllThoughts)
     .get(getThoughtById)
+    .post(createThought)
+
+    //thoughts by id
+  router
+    .route('/:id')
+    .put(updateOneThought)
+    .delete(deleteThought)  
 
     module.exports = router;
